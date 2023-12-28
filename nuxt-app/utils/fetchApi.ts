8a2871095ad60e155bc7a4dcf3e350ccfabc2b66
MasterZer0o <kubapod1234@wp.pl>
2023-12-28 21:26:37 +0100

    p

--- nuxt-app/utils/fetchApi.ts
+++ nuxt-app/utils/fetchApi.ts
@@ -1,6 +1,9 @@
 export function fetchApi<T>(path: Parameters<typeof $fetch>[0], opts?: Parameters<typeof $fetch>[1]) {
   return $fetch<T>(`${useRuntimeConfig().public.API_BASE}${path}`, {
     credentials: 'include',
-    ...opts
+    ...opts,
+    headers: {
+      cookie: useCookie('session').value ? `session=${useCookie('session').value}` : undefined!
+    },
   })
 }
